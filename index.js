bb5d898e983b571b753f7dfcbc21f200d69bee9c
Tim Kluge <timklge@wh2.tu-dresden.de>
2013-12-08 15:29:45 +0100

    Changed license to the beerware license (which is similar to WTFPL)

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <timklge@wh2.tu-dresden.de> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return - Tim Kluge
+ * ----------------------------------------------------------------------------
+ */
+
 var net = require("net"),
 	LineInputStream = require("line-input-stream"),
 	events = require("events"),
